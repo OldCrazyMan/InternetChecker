503dc5c1d69658535703965d3c843bbee0c9ec6a
Tim Akhm <akhmetov.t@techspaceapp.com>
2025-11-06 14:00:41 +0200

    feat: network monitoring and localization setup
    - Add NWPathMonitor for connectivity tracking
    - Implement multi-language support (en/ru)
    - Create notification system for network events

---
+++ InternetChecker/NetworkMonitor.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkMonitor.swift
+//  InternetChecker
+//
+//  Created by Tim Akhmetov on 06.11.2025.
+//
+
+import Foundation
---
+++ InternetChecker/Resources/Extensions.swift
@@ -0,0 +1,23 @@
+//
+//  Extensions.swift
+//  InternetChecker
+//
+//  Created by Tim Akhmetov on 06.11.2025.
+//
+
+import Foundation
+import Network
+
+extension Notification.Name {
+    static let connectivityStatus = Notification.Name(rawValue: "connectivityStatusChanged")
+}
+
+extension NWInterface.InterfaceType: CaseIterable {
+    public static var allCases: [NWInterface.InterfaceType] = [
+        .other,
+        .wifi,
+        .cellular,
+        .loopback,
+        .wiredEthernet
+    ]
+}
